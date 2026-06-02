5e5a23479c4e616d1d36fb76bc734f0541304298
Ilya <northenligth6899@gmail.com>
2026-06-02 15:40:42 +0300

    changes in GET tests

--- tests/test_get_bookings.py
+++ tests/test_get_bookings.py
@@ -19,7 +19,7 @@ def test_bookings_id(booking_auth_client, booking_sample_create_model):
         raise Exception()
 
     bookingid = resp.json()['bookingid']
-    resp = booking_auth_client.get_booking_id(str(bookingid))
+    resp = booking_auth_client.get_booking_id(bookingid)
     assert resp.status_code == 200
 
 
@@ -28,7 +28,14 @@ def test_bookings_id(booking_auth_client, booking_sample_create_model):
 @allure.title("Проверка чтения c ID типа str")
 @pytest.mark.parametrize('booking_id', ['abc'])
 def test_booking_id_negative(booking_client, booking_id):
-    resp = booking_client.get_booking_id(str(booking_id))
+    resp = booking_client.get_booking_id(booking_id)
     assert resp.status_code == 404
-    
-    
+
+
+@allure.feature("API")
+@allure.story("Read")
+@allure.title("Проверка чтения по несуществующему ID")
+def test_get_booking_not_exists(booking_client):
+    bookingid = 99999
+    resp = booking_client.get_booking_id(bookingid)
+    assert resp.status_code == 404
